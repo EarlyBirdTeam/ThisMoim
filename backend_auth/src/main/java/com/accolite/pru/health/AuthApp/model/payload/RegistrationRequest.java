1160ddb7c356b4ccd78153b889269d319bbaeb91
배재원 <pageone0820@naver.com>
2020-08-07 00:23:54 +0900

    register parameter fixed

--- backend_auth/src/main/java/com/accolite/pru/health/AuthApp/model/payload/RegistrationRequest.java
+++ backend_auth/src/main/java/com/accolite/pru/health/AuthApp/model/payload/RegistrationRequest.java
@@ -51,46 +51,4 @@ public class RegistrationRequest {
             dataType = "boolean", allowableValues = "true, false")
     private Boolean registerAsAdmin;
 
-//    public RegistrationRequest(String username, String email,
-//                               String password, Boolean registerAsAdmin) {
-//        this.username = username;
-//        this.email = email;
-//        this.password = password;
-//        this.registerAsAdmin = registerAsAdmin;
-//    }
-//
-//    public RegistrationRequest() {
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public Boolean getRegisterAsAdmin() {
-//        return registerAsAdmin;
-//    }
-//
-//    public void setRegisterAsAdmin(Boolean registerAsAdmin) {
-//        this.registerAsAdmin = registerAsAdmin;
-//    }
 }
