4090194398ad0d18b3ce2af7bdf79894dda9c2ed
최문경 <chl30shl20@gmail.com>
2020-08-06 14:25:48 +0000

    Feature/1

--- backend_board/src/main/java/com/websocket/board/service/DBSyncService.java
+++ backend_board/src/main/java/com/websocket/board/service/DBSyncService.java
@@ -1,11 +1,12 @@
 package com.websocket.board.service;
 
-import com.websocket.board.dto.Channel;
 import com.websocket.board.model.SocketBoardMessage;
 
 public interface DBSyncService {
 
     void postitDBSync(SocketBoardMessage board);
+    void postitDeleteSync(SocketBoardMessage board);
+
     void channelDBIdCountSync(String channelId, Long idCount);
 
 }
