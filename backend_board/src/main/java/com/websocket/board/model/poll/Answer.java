1e411bbf4d05aee4712ef43b2967636641ce43bf
MUNKYUNG CHOI <chl30shl20@gmail.com>
2020-08-11 12:20:08 +0900

    feat #14 Create 'Poll' structure

---
+++ backend_board/src/main/java/com/websocket/board/model/poll/Answer.java
@@ -0,0 +1,15 @@
+package com.websocket.board.model.poll;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Answer {
+    private String answer;
+    private int voted;
+}
