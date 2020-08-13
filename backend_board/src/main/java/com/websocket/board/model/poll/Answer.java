01965d5ca237040c204d4efa2634e86f37bc46fd
MUNKYUNG CHOI <chl30shl20@gmail.com>
2020-08-13 15:11:28 +0900

    bugfix #11 Add 'Poll > Answer' class Serializable

--- backend_board/src/main/java/com/websocket/board/model/poll/Answer.java
+++ backend_board/src/main/java/com/websocket/board/model/poll/Answer.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
-public class Answer {
+public class Answer implements Serializable {
     private String answer;
     private int voted;
 }
