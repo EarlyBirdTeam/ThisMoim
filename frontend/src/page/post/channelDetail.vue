13ca8f2bcd7e88bba23cde43da1b4245711912c2
ruylruyl <44992114+dongryul123@users.noreply.github.com>
2020-08-18 16:01:05 +0900

    getting moim memberList

--- frontend/src/page/post/channelDetail.vue
+++ frontend/src/page/post/channelDetail.vue
@@ -141,19 +141,16 @@
           </div>
 
           <div class="my-3 p-3 bg-white rounded shadow-sm">
-            <h2 class="border-bottom border-gray pb-2 mb-0">OOO모임</h2>
-            <div class="media text-muted pt-3">
+            <h2 class="border-bottom border-gray pb-2 mb-0">OOO모임 </h2>
+            
+            <div class="media text-muted pt-3"
+              v-for="(member, idx) in board.memberList"
+              :key="idx"
+              >
               <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
               <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                <strong class="d-block text-gray-dark" style="text-align:left; font-size:30px;">회원1</strong>
+                <strong class="d-block text-gray-dark" style="text-align:left; font-size:30px;">{{ member }}</strong>
                 <p style="text-align:right;"> 모임원 </p>     
-              </p>
-            </div>
-            <div class="media text-muted pt-3">
-              <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-              <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                <strong class="d-block text-gray-dark" style="text-align:left; font-size:30px;">회원2</strong>     
-                <p style="text-align:right;"> 모임원 </p> 
               </p>
             </div>
           </div>
